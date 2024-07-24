a6340a91c1e3c1c99c60aa2dad23572469a4318a
Kaustubha Ram <kaustubharam2004@gmail.com>
2024-07-24 14:58:17 +0530

    update

--- Kaustubha Ram questions answered.py
+++ Kaustubha Ram questions answered.py
@@ -13,14 +13,17 @@ db_params = {
 conn = psycopg2.connect(**db_params)
 cur = conn.cursor()
 
-print("Question1")
-count_orders_query = "SELECT COUNT(*) FROM orders"
-try:
-    cur.execute(count_orders_query)
-    total_orders = cur.fetchone()[0]
-    print(f"Total number of orders placed: {total_orders}")
-except Exception as e:
-    print(f"Error executing query: {e}")
+def execute_query(query, conn):
+    with conn.cursor() as cur:
+        cur.execute(query)
+        result = cur.fetchall()
+    return result
+
+# Q1: The total number of orders placed
+query1 = "SELECT COUNT(DISTINCT order_id) AS total_orders FROM orders;"
+total_orders = execute_query(query1, conn)
+print("Question1: ")
+print("Total number of orders placed:", total_orders[0][0])
 
 
 
@@ -34,6 +37,23 @@ JOIN pizza p ON od.pizza_id = p.pizza_id
 try:
     # Execute the query
     cur.execute(total_revenue_query)
+    # Fetch the result
+    total_revenue = cur.fetchone()[0]
+    print(f"Total revenue generated from pizza sales: ${total_revenue:.2f}")
+except Exception as e:
+    print(f"Error executing query: {e}")
+
+print("Question3")
+highest_price_pizza = """
+select * from
+(select * from pizza_type pt
+inner join pizza p on
+pt.pizza_type_id=p.pizza_type_id
+"""
+
+try:
+    # Execute the query
+    cur.execute(highest_price_pizza)
     # Fetch the result
     total_revenue = cur.fetchone()[0]
     print(f"Total revenue generated from pizza sales: ${total_revenue:.2f}")
